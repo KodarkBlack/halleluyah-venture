77a00c7d5d5794e57bd1ef9fe8ab7d880a22217e
Olufisayo Israel Tomide <115098511+KodarkBlack@users.noreply.github.com>
2023-08-14 15:17:33 +0100

    edit

--- fun.js
+++ fun.js
@@ -42,3 +42,48 @@ function searchWorkers() {
 
     updateWorkerList(filteredWorkers);
 }
+
+
+// 
+
+function updateWorkerList(filteredWorkers = workers) {
+    const workerList = document.getElementById('workerList');
+    workerList.innerHTML = '';
+
+    filteredWorkers.forEach((worker, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${worker.name}</td>
+            <td>${worker.foodItems}</td>
+            <td>${worker.amount}</td>
+            <td>
+                <button onclick="editWorker(${index})">Edit</button>
+                <button onclick="deleteWorker(${index})">Delete</button>
+            </td>
+        `;
+        workerList.appendChild(row);
+    });
+}
+
+function editWorker(index) {
+    const worker = workers[index];
+    const newName = prompt('Enter new name:', worker.name);
+    const newFoodItems = prompt('Enter new food items:', worker.foodItems);
+    const newAmount = prompt('Enter new amount:', worker.amount);
+
+    if (newName && newFoodItems && newAmount !== null) {
+        worker.name = newName;
+        worker.foodItems = newFoodItems;
+        worker.amount = newAmount;
+        updateWorkerList();
+    }
+}
+
+function deleteWorker(index) {
+    const confirmDelete = confirm('Are you sure you want to delete this worker?');
+
+    if (confirmDelete) {
+        workers.splice(index, 1);
+        updateWorkerList();
+    }
+}
